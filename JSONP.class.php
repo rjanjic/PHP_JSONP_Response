e156ab0671209f855c04dabdfb12a46030000281
Radovan Janjic <rade@it-radionica.com>
2014-03-21 10:39:54 +0100

    no cache update

--- JSONP.class.php
+++ JSONP.class.php
@@ -113,7 +113,11 @@ class JSONP {
 		
 		if ($print) {
 			if ($header) {
-				header('Cache-Control: no-cache, must-revalidate');
+				header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+				header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+				header("Cache-Control: no-store, no-cache, must-revalidate");
+				header("Cache-Control: post-check=0, pre-check=0", FALSE);
+				header("Pragma: no-cache");
 				header('Content-Type: application/' . ($jsonp ? 'javascript' : 'json'));
 			}
 			echo $return;
